316d1da32d1303eb26ff9119202728338e194131
mehak <mehakkanyal@gmail.com>
2025-03-13 23:37:27 +0530

    Updated code

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,4 @@
 
-
-/*
-Variable naming convention: <object>_<action>_<objectname>; Example -> Button_click_b1;
-*/
-
-//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
 var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
 var inp_gen=document.getElementById("a_generate");
 var inp_aspeed=document.getElementById("a_speed");
@@ -87,9 +81,3 @@ function runalgo()
     }
 }
 
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
